604692b28862dc1ce35ebd9efc2810169c0411e5
EchoFoe <Aspirpd@gmail.com>
2019-02-21 01:10:42 +0300

    version#5

--- jobs/admin.py
+++ jobs/admin.py
@@ -5,6 +5,7 @@ from import_export.admin import ImportExportActionModelAdmin
 from import_export import resources
 from import_export import fields
 from import_export.widgets import ForeignKeyWidget
+from django.urls import reverse
 
 
 class JobImageInline(admin.TabularInline):
